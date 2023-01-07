ef5723d6470ddd81d9f6b6be4c03e0ebe670bff7
kenty02 <42216299+kenty02@users.noreply.github.com>
2023-01-07 14:26:28 +0900

    use pnpm, add script to gen trpc server types for client, add prettier

---
+++ README.md
@@ -0,0 +1,8 @@
+# Clay Host
+
+## trpcクライアント向け型生成
+
+```shell
+pnpm run gen-types
+mv server.d.ts /mnt/c/Users/kenty/dev/clay-viewer-electron/src/ # change dir name
+```
--- src/handlers/commands.ts
+++ src/handlers/commands.ts
@@ -1,14 +1,14 @@
-import browser from "webextension-polyfill";
-import {notifyUser} from "../background/utils";
+import browser from 'webextension-polyfill'
+import { notifyUser } from '../background/utils'
 
 export const handleCommands = () => {
   browser.commands.onCommand.addListener((command) => {
-    (async () => {
-      if (command === "open-view") {
-        void notifyUser(`sorry, "${command}" is not implemented yet`);
+    ;(async () => {
+      if (command === 'open-view') {
+        void notifyUser(`sorry, "${command}" is not implemented yet`)
       } else {
-        void notifyUser(`sorry, "${command}" is not implemented yet`);
+        void notifyUser(`sorry, "${command}" is not implemented yet`)
       }
-    })().catch(console.error);
-  });
-};
+    })().catch(console.error)
+  })
+}
