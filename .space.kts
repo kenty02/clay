540338b860ceecf35cf7e51c56d0ae12d7c0ec97
kenty02 <42216299+kenty02@users.noreply.github.com>
2023-01-07 15:31:26 +0900

    add ci

---
+++ .space.kts
@@ -0,0 +1,29 @@
+/**
+ * JetBrains Space Automation
+ * This Kotlin-script file lets you automate build activities
+ * For more info, see https://www.jetbrains.com/help/space/automation.html
+ */
+
+job("Build for Chromium-based browsers") {
+    container(image = "node:16-alpine") {
+        shellScript {
+            content = """
+                echo Install build deps
+                apk update
+                apk add --no-cache zip
+                
+                echo Building (without type-checking and signing)
+                npm install -g pnpm
+                pnpm install
+                npx vite build # TODO: use pnpm run build
+
+                echo Done, zipping and uploading
+                zip -r clay-host.zip dist/
+                SOURCE_PATH=clay-host-${'$'}JB_SPACE_EXECUTION_NUMBER.zip
+                TARGET_PATH=clay-host/
+                REPO_URL=https://files.pkg.jetbrains.space/npathy/p/clay/files
+                curl -i -H "Authorization: Bearer ${'$'}JB_SPACE_CLIENT_TOKEN" -F file=@"${'$'}SOURCE_PATH" ${'$'}REPO_URL/${'$'}TARGET_PATH
+            """.trimIndent()
+        }
+    }
+}
