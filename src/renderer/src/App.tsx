c2be84aa2f8d58641c644fe98cad2a1a0c04e938
kenty02 <42216299+kenty02@users.noreply.github.com>
2023-01-17 16:14:20 +0900

    support renderer debug from webstorm, add mantine notifications & react-query-devtools & error boundary

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,15 +1,50 @@
-import { MantineProvider, Text } from '@mantine/core'
+import { Button, MantineProvider, Text } from '@mantine/core'
 import NodeTableView from './components/NodeTableView'
 import TrpcProvider from './providers/TrpcProvider'
+import { Suspense } from 'react'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { NotificationsProvider } from '@mantine/notifications'
+import { ErrorBoundary, FallbackProps } from 'react-error-boundary'
 
 function App(): JSX.Element {
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
-      <TrpcProvider>
-        <Text>Welcome to Mantine</Text>
-        <NodeTableView />
-      </TrpcProvider>
+      <NotificationsProvider position={'top-right'}>
+        <Suspense fallback={<Text>Loading...</Text>}>
+          <TrpcProvider>
+            <ErrorBoundary
+              FallbackComponent={ErrorFallback}
+              onReset={(): void => {
+                // reset the state of your app so the error doesn't happen again
+              }}
+            >
+              <Text>Welcome to Mantine</Text>
+              <Button
+                onClick={(): void => {
+                  location.reload()
+                }}
+              >
+                Reload
+              </Button>
+              <Suspense fallback={<Text>Loading...</Text>}>
+                <NodeTableView />
+              </Suspense>
+            </ErrorBoundary>
+            <ReactQueryDevtools initialIsOpen={false} />
+          </TrpcProvider>
+        </Suspense>
+      </NotificationsProvider>
     </MantineProvider>
+  )
+}
+
+function ErrorFallback({ error, resetErrorBoundary }: FallbackProps): JSX.Element {
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre>{error.message}</pre>
+      <button onClick={resetErrorBoundary}>Try again</button>
+    </div>
   )
 }
 
