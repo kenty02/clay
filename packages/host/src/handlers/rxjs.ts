79ec9a11976c270d4f6963a1e7b2047572decca6
kenty02 <42216299+kenty02@users.noreply.github.com>
2023-01-30 13:55:17 +0900

    remove chrome event mock

--- packages/host/src/handlers/rxjs.ts
+++ packages/host/src/handlers/rxjs.ts
@@ -1,11 +1,11 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
-import { fromEventPattern, merge, Observable, Observer, Subject } from 'rxjs'
+import { fromEventPattern, Observable } from 'rxjs'
 
 function fromChromeEvent<
   T extends chrome.events.Event<(...args: unknown[]) => void>,
   R = Parameters<Parameters<T['addListener']>[0]>
->(event: T, key: string): Observable<R> & { mock: Observer<R> } {
-  const original = fromEventPattern(
+>(event: T): Observable<R> {
+  return fromEventPattern(
     (handler) => {
       event.addListener(handler)
     },
@@ -14,22 +14,16 @@ function fromChromeEvent<
     },
     (...args) => args as R
   )
-
-  const mock = new Subject<R>()
-  const merged = merge(original, mock)
-  return Object.defineProperty(merged, 'mock', { value: mock }) as Observable<R> & {
-    mock: Observer<R>
-  }
 }
 
 export const tabs = {
   get activationStream() {
-    return fromChromeEvent(chrome.tabs.onActivated, 'tabs.onActivated')
+    return fromChromeEvent(chrome.tabs.onActivated)
   }
 }
 
 export const runtime = {
   get messageExternalStream() {
-    return fromChromeEvent(chrome.runtime.onMessageExternal, 'runtime.onMessageExternal')
+    return fromChromeEvent(chrome.runtime.onMessageExternal)
   }
 }
